9e6f4d8de7ff31dcae5a1523817cb821aa33a568
Budleigh Salterton @ Zagato <budleigh.salterton@gmail.com>
2017-01-28 20:07:18 +0100

    somewhat working

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,32 @@
 $(function() {
 
     /* globals io, Handlebars */
+    let seq = 0;
 
     const tpl = Handlebars.compile($("#card-template").html());
+    const liveQuestions = {};
 
     $("body").on("mousewheel", function(event, delta) {
         this.scrollLeft -= (delta * 30);
         event.preventDefault();
     });
 
-
-
     const socket = io('/');
+    $("#questions").on("click", ".question .btn", (btn) => {
+        const but = $(btn.target);
+        const queryId = but.parents('.question').data().queryid;
+        const ret = but.data().index;
+
+        socket.emit("answer", queryId, ret);
+    });
+
     socket.on('connect', () => {});
     socket.on('inquiry', (data) => {
-        console.log("data", data);
-        $("questions").append(
-            tpl(data)
-        );
+        liveQuestions[data.queryId] = data.cardId = 'ref-' + seq++;
+        $("#questions").append(tpl(data));
     });
     socket.on('outquiry', (id) => {
-        $('#inq-'+id).remove();
+        $('#' +liveQuestions[id]).remove();
     });
     socket.on('disconnect', () => {});
 
