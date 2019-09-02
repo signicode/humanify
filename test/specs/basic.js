0ccf9ab5470125c3b5dd3e748db439babaeffd19
MichalCz <cz@signicode.com>
2019-09-02 16:51:57 +0000

    Tests for firefox and chrome

---
+++ test/specs/basic.js
@@ -0,0 +1,63 @@
+const assert = require('assert');
+const os = require("os");
+const host = os.hostname();
+
+const streams = {};
+
+beforeAll(async () => {
+    const [input, output] = await require("../lib/start-humanify")();
+    Object.assign(streams, {input, output});
+})
+
+describe('Humanify App', () => {
+
+    it('should pass the minimal example', async () => {
+        await browser.url(`http://${host}:3333/`);
+
+        const title = await browser.getTitle();
+        assert.strictEqual(title, 'Humanify App');
+
+        assert.strictEqual(await browser.execute(() => document.querySelector('#questions div')), null);
+
+        await streams.input.whenWrote({content: "hello world", user: "someone123"});
+
+        await new Promise(r => setTimeout(r, 100));
+
+        assert.strictEqual(await browser.execute(() => document.querySelector('#questions div .card-content').textContent.trim()), "hello world");
+        assert.strictEqual(await browser.execute(() => document.querySelector('#questions div .card-title').textContent.trim()), "someone123");
+
+        const buttons = await browser.execute(() => Array
+            .from(document.querySelectorAll('#questions div:first-child button'))
+            .map(btn => [Array.from(btn.classList), btn.innerHTML])
+        );
+
+        assert.deepStrictEqual(
+            [[['btn', 'btn-main'], 'add'], [['btn', 'btn-danger'], 'del'], ],
+            buttons
+        );
+
+        await browser.execute(() => document.querySelector('#questions div:first-child button.btn-main').dispatchEvent(new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true,
+            view: window
+        })));
+
+        await new Promise(r => setTimeout(r, 100));
+
+        const item = streams.output.read();
+
+        assert.deepStrictEqual(
+            item,
+            {
+                answer: 1,
+                item: {
+                  content: 'hello world',
+                  user: 'someone123'
+                }
+            }
+        );
+
+    });
+
+    it.skip('should pass a multi-root example')
+});
